560e8e6bd4ddb9910bc4e6265572a525f5c54755
Mocha <LegalMustela@gmail.com>
2023-09-13 01:05:01 -0400

    changes to readme + basic plugin functionality

--- README.md
+++ README.md
@@ -7,25 +7,25 @@ This helix-inspired plugin for [Obsidian](https://obsidian.md/) that simplifies 
 - Customizable Google Docs inspired Hotkeys
 ## Installation
 - Download the Latest Release from the Releases section of the GitHub Repository
-- Extract the plugin folder from the zip to your vault's plugins folder: `<vault>/.obsidian/plugins/` Note: On some machines the `.obsidian` folder may be hidden. On MacOS you should be able to press `Command+Shift+Dot` to show the folder in Finder.
+- Extract the plugin folder from the zip to your vault's plugins folder: `<vault>/.obsidian/plugins/` Note: On some machines the `.obsidian` folder may be hidden. On MacOS you should be able to press `Cmd + Shift + .` to show the folder in Finder.
 - Reload Obsidian
 ## Usage
 ### Lazy Mode
 - Use `Cmd + Shift + ,` to enter a subscript.
 - Use `Cmd + Shift + .` to enter a superscript.
-- Use `Opt + Tab` or enter `Cmd + Shift + ,` / `Cmd + Shift + .` to exit out of the respective mode.
-### Swift Mode
+- Use `Esc` or enter `Cmd + Shift + ,` / `Cmd + Shift + .` to exit out of the respective mode.
+### Swift Mode (Currently Not Implemented)
 - Select the text manually with your mouse or keyboard. And proceed to:
 	- Set currently selected text to subscript or superscript:
 		- Use `m + s + ,` to surround the current selected text and set to subscript.
 		- Use `m + s + .` to surround the current selected text and set to superscript.
 	- Replace superscript or subscript for one another:
-		- Use `m + r + ,` to replace current script with subscript.
-		- Use `m + r + .` to replace current script with superscript.
+		- Use `m + r + ,` to replace current surrounding script with subscript.
+		- Use `m + r + .` to replace current surrounding script with superscript.
 	- Delete current subscript or superscript:
 		- Use `m + d` to delete the current subscript or superscript.
 ## Settings
 - Choose which mode you’d like to use, Lazy, Swift, or both!
-- Choose to enable Confirm Exit
+- Sub/Sup tags within Sub/Sup tags
 ## Support
 If you want to support me([Moqqa](https://github.com/moqqa)) you can do so on [Ko-fi!](https://ko-fi.com/moqqa)
--- main.ts
+++ main.ts
@@ -1,134 +1,91 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownView, Editor } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class LazyScript extends Plugin {
+  onload() {
+    this.addCommand({
+      id: "toggle-subscript",
+      name: "Enter or Exit <sub> tags",
+      editorCallback: () => this.toggleSubscript(),
+      hotkeys: [
+        {
+          modifiers: ["Mod", "Shift"],
+          key: ",",
+        },
+      ],
+    });
+    this.addCommand({
+      id: "toggle-superscript",
+      name: "Enter or Exit <sup> tags",
+      editorCallback: () => this.toggleSuperscript(),
+      hotkeys: [
+        {
+          modifiers: ["Mod", "Shift"],
+          key: ".",
+        },
+      ],
+    });
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    // Add a universal Escape hotkey with "Esc + S" to exit tags mode
+    this.addCommand({
+      id: "exit-tags-mode",
+      name: "Exit Tags Mode",
+      editorCallback: () => this.exitTagsMode(),
+      hotkeys: [
+        {
+          modifiers: [],
+          key: "Escape",
+        },
+      ],
+    });
+  }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  toggleSubscript() {
+    this.toggleTags("<sub>", "</sub>");
+  }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  toggleSuperscript() {
+    this.toggleTags("<sup>", "</sup>");
+  }
 
-	async onload() {
-		await this.loadSettings();
+  exitTagsMode() {
+    const editor = this.getEditorIfMarkdownView();
+    if (!editor) return;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    const cursorPos = editor.getCursor();
+    const lineText = editor.getLine(cursorPos.line);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // Check if the cursor is inside a tag (either <sub> or <sup>)
+    if (lineText.includes("<sub>") || lineText.includes("<sup>")) {
+      // Move the cursor to the end of the line to exit the tags mode
+      editor.setCursor(cursorPos.line, lineText.length);
+    }
+  }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  toggleTags(openTag: string, closeTag: string) {
+    const editor = this.getEditorIfMarkdownView();
+    if (!editor) return;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    const cursorPos = editor.getCursor();
+    const lineText = editor.getLine(cursorPos.line);
+    const isInsideTag = lineText.includes(openTag) && lineText.includes(closeTag);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    if (isInsideTag) {
+      // Cursor is inside an existing tag; move cursor to the end of the text within the tag
+      const startIndex = lineText.indexOf(openTag);
+      const endIndex = lineText.lastIndexOf(closeTag);
+      if (startIndex !== -1 && endIndex !== -1) {
+        editor.setCursor(cursorPos.line, endIndex + closeTag.length);
+      }
+    } else {
+      // Cursor is not inside a tag; insert tags and move cursor inside the open tag without a space
+      const insertionText = `${openTag}${closeTag}`;
+      editor.replaceSelection(insertionText);
+      editor.setCursor(cursorPos.line, cursorPos.ch + openTag.length); // Move cursor inside the open tag
+    }
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  getEditorIfMarkdownView(): Editor | null {
+    const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+    return view ? view.editor : null;
+  }
 }
